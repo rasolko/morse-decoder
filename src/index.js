fb0dabe57e9faebb57655ba7aefd6a99d5c29328
rasolko <vano2029492@mail.ru>
2020-10-20 23:44:59 +0300

    feat: complete

--- src/index.js
+++ src/index.js
@@ -35,10 +35,37 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    let arr2 = [];
+    let result = '';
+    let charCount = Math.floor(expr.length / 10);
+    for(let i = 0; i < charCount; i++){
+        arr.push(expr.substr(i * 10, 10));
+    }
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === '**********'){
+            arr2.push(' ');
+            continue
+        }
+        let char = '';
+        for(let j = 0; j < arr[i].length; j = j + 2){
+            let twoNumber = arr[i].substr(j, 2);
+            if(twoNumber === '10'){
+                char = char + '.';
+            } else if(twoNumber === '11'){
+                char = char + '-';
+            }
+        }
+        arr2.push(char);
+    }
+    arr2.map( (item, index) => {
+        result = result + MORSE_TABLE[item];
+    })
+    return result
 }
 
 module.exports = {
